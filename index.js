59da74e73fce9538e2e1ff9d443a528e0b1d2faf
MightiD <128378210+MightiD@users.noreply.github.com>
2025-10-08 08:34:40 +0100

    logs in successfully

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 let config = require(process.env.HOME + "/.config/timetable/config.json");
 
-console.log(config);
+let body = `code=${config.code.toUpperCase()}&dob=${config.dob}`
 
 fetch("https://www.classcharts.com/apiv2student/login", {
-    "body": `code=${config.code}&dob=${config.dob}&recaptcha-token=no-token-available`,
+    "headers": {
+        "content-type": "application/x-www-form-urlencoded"
+    },
+    "body": body,
     "method": "POST",
-    "credentials": "include"
 })
 .then(res => {
     if (!res.ok) throw new Error("HTTP Error")
-    return res.json()
+    return res.json();
 })
-.then(data => console.log(data))
+.then(data => {
+    console.log(data)
+})
 .catch(err => console.error("Fetch error:", err));
+
 
