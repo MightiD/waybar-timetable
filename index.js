214d8a57678fe20e4e9fe201c6711bdce7e6045f
MightiD <128378210+MightiD@users.noreply.github.com>
2025-10-08 10:26:51 +0100

    functions are cool

--- index.js
+++ index.js
@@ -2,20 +2,36 @@ let config = require(process.env.HOME + "/.config/timetable/config.json");
 
 let body = `code=${config.code.toUpperCase()}&dob=${config.dob}`
 
-fetch("https://www.classcharts.com/apiv2student/login", {
-    "headers": {
-        "content-type": "application/x-www-form-urlencoded"
-    },
-    "body": body,
-    "method": "POST",
-})
-.then(res => {
-    if (!res.ok) throw new Error("HTTP Error")
-    return res.json();
-})
-.then(data => {
-    console.log(data)
-})
-.catch(err => console.error("Fetch error:", err));
+async function getLoginToken() {
 
+    let login;
 
+    await fetch("https://www.classcharts.com/apiv2student/login", {
+        "headers": {
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        "body": body,
+        "method": "POST",
+    })
+    .then(res => {
+        if (!res.ok) throw new Error("HTTP Error")
+        return res.json();
+    })
+    .then(data => {
+        login = data;
+    })
+    .catch(err => console.error("Fetch error:", err));
+
+    return login
+}
+
+async function main() {
+    let token = await getLoginToken();
+    console.log(token);
+}
+
+main()
+.catch(err => {
+    console.error("Error in main: ", err);
+    process.exit(1);
+})
